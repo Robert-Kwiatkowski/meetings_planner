95d83042541fb1ead64837f15ab75a36b536329f
Robert-Kwiatkowski <s169655@student.pg.edu.pl>
2020-04-09 23:39:23 +0200

    Added templates

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from meetings.models import Meeting
+
 
 def welcome(request):
-    return HttpResponse("Welcome to the meetings planner")
+    return render(request, "website/welcome.html",
+                  {"num_meetings": Meeting.objects.count()})
